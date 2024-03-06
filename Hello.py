2acb2919d64f92e671bbec37787ea83a2df2c2ac
HaoyuWangEBSCO <146026568+HaoyuWangEBSCO@users.noreply.github.com>
2024-03-06 10:30:42 -0500

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -22,23 +22,35 @@ def run():
     st.set_page_config(
         page_title="Compare Citation Based Recommendation and AI based",
         page_icon="👋",
-
+    
     )
     st.header("Citation VS AI keyword",divider='rainbow')
-    st.write('These following recommendation engines are POCs that leverages different form of technology build by SA Frank and the Cortext Team. Due to the nature of the underlying logic of the citation-based recommendation, the current evaluation method could not accurately measure the perofmanece of this new recommendation.
-    Therefore, to get a better understanding the the performance of the citation based recommendation, we decided to take a human evaluation approach')
+    st.write("""These following recommendation engines are POCs that leverages different form of technology build by SA Frank and the Cortext Team. Due to the nature of the underlying logic of the citation-based recommendation, the current evaluation method could not accurately measure the perofmanece of this new recommendation.
+        Therefore, to get a better understanding the the performance of the citation based recommendation, we decided to take a human evaluation approach""")
     st.divider()
-    st.subheader ('Rating Scale')
+    st.subheader (':blue[Rating Scale]')
     
-    st.write("Please rate these on a scale of 0-2,
-    0 as the result is not relevant to the target record,
-    1 as the result is covering a single concept from the target record or an average recommendation, 
-    2 as the result is good recommendation or covering two or all main concepts of the target record.")
+    st.write("""Please rate these on a scale of 0-2:""")
+    st.write(""":red[[0]] as the result is a bad recommendation that is not relevant to the target record""")
+    st.write(""":blue[[1]] as the result is an average recommendation that covers at least a single concept from the target record""")
+    st.write(""" :green[[2]] as the result is a good recommendation that covers two or all main concepts of the target record.""")
     
+    st.write('Please rate the top 5 records from each engine')
     st.divider()
     st.subheader ('Recommendation Engines')
-    st.write(Citation Based Recommendation
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.header("Citation Based POC")
+        st.write("""This is the citation based POC developed in [F76700](https://rally1.rallydev.com/#/?detail=/portfolioitem/feature/720287977253&fdp=true),which is a canidate for the recommendation engine 3.0.
+                 This engine leverages the citing and refering relationship of the target record to find relevant record"""
+                 )
     
+    with col2:
+        st.header("AI Key Word Based")
+        st.write("These are AI keyword extraction based recommendation engine. Separatly, GPT 4 and GPT 3.5 turbo are used to extract key words from the record's title and abstract to build an advanced query to identify recommendations.")
+
+
 
 
 
